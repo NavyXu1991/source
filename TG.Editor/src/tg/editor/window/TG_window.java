ec595d72ade209f144f563f12cd3e2c37c1a9ba7
navy xu <navy xu>
2016-01-23 22:55:12 +0800

    m

--- TG.Editor/src/tg/editor/window/TG_window.java
+++ TG.Editor/src/tg/editor/window/TG_window.java
@@ -68,70 +68,70 @@ public class TG_window {
 		mnFile.setIcon(new ImageIcon(TG_window.class.getResource("/tg/editor/window/icon/icon.PNG")));
 		mnFile.setBackground(UIManager.getColor("control"));
 		mnFile.setForeground(Color.BLACK);
-		mnFile.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnFile.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnFile);
 		
 		JMenuItem mntmNewProject = new JMenuItem("New Project");
-		mntmNewProject.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmNewProject.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmNewProject);
 		
 		JMenuItem mntmImportProject = new JMenuItem("Import Project");
-		mntmImportProject.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmImportProject.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmImportProject);
 		
 		mnFile.addSeparator();
 		
 		JMenuItem mntmNewFile = new JMenuItem("New File");
-		mntmNewFile.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmNewFile.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmNewFile);
 		
 		JMenuItem mntmOpen = new JMenuItem("Open...");
-		mntmOpen.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmOpen.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmOpen);
 		
 		JMenuItem mntmSetting = new JMenuItem("Settings");
-		mntmSetting.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmSetting.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmSetting);
 		
 		mnFile.addSeparator();
 		
 		JMenuItem mntmSave = new JMenuItem("Save");
 		mntmSave.setIcon(new ImageIcon(TG_window.class.getResource("/com/sun/java/swing/plaf/windows/icons/FloppyDrive.gif")));
-		mntmSave.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmSave.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmSave);
 		
 		JMenuItem mntmSaveAs = new JMenuItem("Save As...");
-		mntmSaveAs.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmSaveAs.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmSaveAs);
 		
 		JMenuItem mntmSaveAll = new JMenuItem("Save All");
-		mntmSaveAll.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmSaveAll.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmSaveAll);
 		
 		JMenuItem mntmRefresh = new JMenuItem("Refresh");
-		mntmRefresh.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmRefresh.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmRefresh);
 		
 		mnFile.addSeparator();
 		
 		JMenuItem mntmImportSettings = new JMenuItem("Import Settings");
-		mntmImportSettings.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmImportSettings.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmImportSettings);
 		
 		JMenuItem mntmExportSettings = new JMenuItem("Export Settings");
-		mntmExportSettings.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmExportSettings.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmExportSettings);
 		
 		mnFile.addSeparator();
 		
 		JMenuItem mntmPrint = new JMenuItem("Print...");
-		mntmPrint.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmPrint.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFile.add(mntmPrint);
 		
 		mnFile.addSeparator();
 		
 		JMenuItem mnQuit = new JMenuItem("Quit");
-		mnQuit.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mnQuit.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnQuit.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent e) {
 				frame.dispose();
@@ -145,152 +145,152 @@ public class TG_window {
 		
 		JMenu mnEdit = new JMenu("  Edit  ");
 		mnEdit.setForeground(Color.BLACK);
-		mnEdit.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnEdit.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnEdit);
 		
 		JMenuItem mntmUndo = new JMenuItem("Undo...");
 		mntmUndo.setIcon(new ImageIcon(TG_window.class.getResource("/com/sun/javafx/scene/web/skin/Undo_16x16_JFX.png")));
-		mntmUndo.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmUndo.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mntmUndo);
 		
 		JMenuItem mntmRedo = new JMenuItem("Redo...");
 		mntmRedo.setIcon(new ImageIcon(TG_window.class.getResource("/com/sun/javafx/scene/web/skin/Redo_16x16_JFX.png")));
-		mntmRedo.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmRedo.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mntmRedo);
 		
 		mnEdit.addSeparator();
 		
 		JMenuItem mntmCut = new JMenuItem("Cut...");
 		mntmCut.setIcon(new ImageIcon(TG_window.class.getResource("/com/sun/javafx/scene/web/skin/Cut_16x16_JFX.png")));
-		mntmCut.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmCut.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mntmCut);
 		
 		JMenuItem mntmCopy = new JMenuItem("Copy");
 		mntmCopy.setIcon(new ImageIcon(TG_window.class.getResource("/com/sun/javafx/scene/web/skin/Copy_16x16_JFX.png")));
-		mntmCopy.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmCopy.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mntmCopy);
 		
 		JMenuItem mntmPaste = new JMenuItem("Paste");
 		mntmPaste.setIcon(new ImageIcon(TG_window.class.getResource("/com/sun/javafx/scene/web/skin/Paste_16x16_JFX.png")));
-		mntmPaste.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmPaste.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mntmPaste);
 		
 		JMenuItem mntmDelet = new JMenuItem("Delete");
-		mntmDelet.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmDelet.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mntmDelet);
 		
 		mnEdit.addSeparator();
 		
 		JMenu mnFind = new JMenu("Find...");
-		mnFind.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mnFind.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnEdit.add(mnFind);
 		
 		JMenuItem mntmFind = new JMenuItem("Find");
-		mntmFind.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmFind.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnFind.add(mntmFind);
 		
 		
 		//----------------------------------------------------------------------------------
 		JMenu mnNavigate = new JMenu("  Navigate  ");
 		mnNavigate.setForeground(Color.BLACK);
-		mnNavigate.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnNavigate.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnNavigate);
 		
 		
 		//----------------------------------------------------------------------------------
 		JMenu mnSearch = new JMenu("  Search  ");
 		mnSearch.setForeground(Color.BLACK);
-		mnSearch.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnSearch.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnSearch);
 		new Menu().File(mnSearch);
 		
 		JMenu mnProgect = new JMenu("  Progect  ");
 		mnProgect.setForeground(Color.BLACK);
-		mnProgect.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnProgect.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnProgect);
 		
 		JMenuItem mntmOpenProgect = new JMenuItem("Open Progect");
-		mntmOpenProgect.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmOpenProgect.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnProgect.add(mntmOpenProgect);
 		
 		JMenuItem mntmCloseProgect = new JMenuItem("Close Progect");
-		mntmCloseProgect.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmCloseProgect.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnProgect.add(mntmCloseProgect);
 		
 		mnProgect.addSeparator();
 		
 		JMenuItem mntmBuild = new JMenuItem("Build");
-		mntmBuild.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmBuild.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnProgect.add(mntmBuild);
 		
 		JMenuItem mntmBuildAll = new JMenuItem("Build All");
-		mntmBuildAll.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmBuildAll.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnProgect.add(mntmBuildAll);
 		
 		mnProgect.addSeparator();
 		
 		JMenuItem mntmClear = new JMenuItem("Clear");
-		mntmClear.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmClear.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnProgect.add(mntmClear);
 		
 		mnProgect.addSeparator();
 		
 		JMenuItem mntmProperty = new JMenuItem("Property");
-		mntmProperty.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmProperty.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnProgect.add(mntmProperty);
 		//----------------------------------------------------------------------------------
 		JMenu mnRun = new JMenu("  Run  ");
 		mnRun.setForeground(Color.BLACK);
-		mnRun.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnRun.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnRun);
 		
 		JMenuItem mntmRun = new JMenuItem("Run");
-		mntmRun.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmRun.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnRun.add(mntmRun);
 		
 		mnRun.addSeparator();
 		
 		JMenu mnRunHistory = new JMenu("Run History");
-		mnRunHistory.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mnRunHistory.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnRun.add(mnRunHistory);
 		
 		JMenu mnRunMode = new JMenu("Run Mode");
-		mnRunMode.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mnRunMode.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnRun.add(mnRunMode);
 		
 		mnRun.addSeparator();
 		
 		JMenuItem mntmRunConfiguratio = new JMenuItem("Run Configuratio");
-		mntmRunConfiguratio.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmRunConfiguratio.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnRun.add(mntmRunConfiguratio);
 		//------------------------------------------------------------
 		JMenu mnWindow = new JMenu("  Window  ");
 		mnWindow.setForeground(Color.BLACK);
-		mnWindow.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnWindow.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnWindow);
 		
 		JMenuItem mntmNewWindow = new JMenuItem("New Window");
-		mntmNewWindow.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmNewWindow.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnWindow.add(mntmNewWindow);
 		
 		JMenuItem mntmNewTgEditor = new JMenuItem("New TG Editor");
-		mntmNewTgEditor.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmNewTgEditor.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnWindow.add(mntmNewTgEditor);
 		
 		mnWindow.addSeparator();
 		
 		JMenuItem mntmCloseWindow = new JMenuItem("Close Window");
-		mntmCloseWindow.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmCloseWindow.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnWindow.add(mntmCloseWindow);
 		
 		JMenuItem mntmCloseTgEditor = new JMenuItem("Close TG Editor");
-		mntmCloseTgEditor.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmCloseTgEditor.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnWindow.add(mntmCloseTgEditor);
 		
 		mnWindow.addSeparator();
 		
 		JMenu mnEditorTabs = new JMenu("Editor Tabs");
-		mnEditorTabs.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mnEditorTabs.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnWindow.add(mnEditorTabs);
 		
 		mnWindow.addSeparator();
@@ -298,33 +298,33 @@ public class TG_window {
 		//----------------------------------------------------------------------------------
 		JMenu mnHelp = new JMenu(" Help  ");
 		mnHelp.setForeground(Color.BLACK);
-		mnHelp.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 16));
+		mnHelp.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		menuBar.add(mnHelp);
 		
 		JMenuItem mntmTgEdit = new JMenuItem("TG Edit");
-		mntmTgEdit.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmTgEdit.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnHelp.add(mntmTgEdit);
 		
 		mnHelp.addSeparator();
 		
 		JMenuItem mntmHelpContent = new JMenuItem("Help Content");
-		mntmHelpContent.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmHelpContent.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnHelp.add(mntmHelpContent);
 		
 		JMenuItem mntmSearch = new JMenuItem("Search");
-		mntmSearch.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmSearch.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnHelp.add(mntmSearch);
 		
 		mnHelp.addSeparator();
 		
 		JMenuItem mntmCheckForUpdates = new JMenuItem("Check for Updates");
-		mntmCheckForUpdates.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmCheckForUpdates.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnHelp.add(mntmCheckForUpdates);
 		
 		mnHelp.addSeparator();
 		
 		JMenuItem mntmAboutEditor = new JMenuItem("About Editor");
-		mntmAboutEditor.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 14));
+		mntmAboutEditor.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 12));
 		mnHelp.add(mntmAboutEditor);
 	}
 }
